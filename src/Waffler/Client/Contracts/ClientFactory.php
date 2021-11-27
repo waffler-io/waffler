9293e51c7143542aa2be86de17d2eb2e5f7039f4
ErickJMenezes <erickmenezes.dev@gmail.com>
2021-11-27 02:22:17 -0300

    Removed wrong annotations from the method.

--- src/Waffler/Client/Contracts/ClientFactory.php
+++ src/Waffler/Client/Contracts/ClientFactory.php
@@ -13,9 +13,6 @@ interface ClientFactory
     /**
      * Generates the implementation for the given client interface.
      *
-     * @param class-string<TInterfaceType> $interfaceName Fully qualified name of the client interface.
-     * @param array<string, mixed>         $options       GuzzleHttp Client options.
-     *
      * @return TInterfaceType
      * @author ErickJMenezes <erickmenezes.dev@gmail.com>
      */
