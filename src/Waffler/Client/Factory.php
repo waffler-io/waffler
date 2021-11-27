f6676fe2387f70ed23fac93d5be3597c277d10a6
ErickJMenezes <erickmenezes.dev@gmail.com>
2021-11-27 02:38:59 -0300

    Extracted the method call handling to its own class.

--- src/Waffler/Client/Factory.php
+++ src/Waffler/Client/Factory.php
@@ -5,13 +5,11 @@ declare(strict_types = 1);
 namespace Waffler\Client;
 
 
-use BadMethodCallException;
 use GuzzleHttp\Client as GuzzleClient;
 use InvalidArgumentException;
 use ReflectionClass;
 use Waffler\Client\Contracts\ClientFactory;
 use Waffler\Generator\AnonymousClassGenerator;
-use Waffler\Generator\Contracts\MethodCallHandler;
 
 /**
  * Class Client
@@ -20,74 +18,68 @@ use Waffler\Generator\Contracts\MethodCallHandler;
  * @phpstan-template TInterfaceType of object
  * @template-implements ClientFactory<TInterfaceType>
  */
-class Factory implements MethodCallHandler, ClientFactory
+class Factory implements ClientFactory
 {
     /**
      * Client constructor.
      *
      * @param \ReflectionClass<TInterfaceType>           $interface
      * @param \Waffler\Generator\AnonymousClassGenerator $anonymousClassGenerator
-     * @param \Waffler\Client\MethodInvoker              $methodInvoker
+     * @param \Waffler\Client\Proxy<TInterfaceType>      $proxy
      */
     public function __construct(
         private ReflectionClass $interface,
         private AnonymousClassGenerator $anonymousClassGenerator,
-        private MethodInvoker $methodInvoker
+        private Proxy $proxy
     ) {
-        $this->validateInterfaceName();
-    }
-
-    /**
-     * Here's where the generated client calls are
-     * handled and dispatched to guzzle's client.
-     *
-     * @param string                   $name
-     * @param array<int|string, mixed> $arguments
-     *
-     * @return mixed
-     * @throws \ReflectionException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function __call(string $name, array $arguments): mixed
-    {
-        if (!$this->interface->hasMethod($name)) {
-            throw new BadMethodCallException("The method $name is not declared in {$this->interface->getName()}.");
-        }
-
-        return $this->methodInvoker->invoke($this->interface->getMethod($name), $arguments);
+        $this->validateInterface();
     }
 
     /**
      * Factory method to create the client implementation.
      *
-     * @param class-string<TInterfaceType> $interface Fully qualified name of the client interface.
-     * @param array<string, mixed>         $options An array of guzzle http client options.
+     * @param class-string<TInterfaceType> $interfaceName Fully qualified name of the client interface.
+     * @param array<string, mixed>         $options       An array of guzzle http client options.
      *
      * @return TInterfaceType
      * @throws \ReflectionException
      * @throws \Exception
-     * @author   ErickJMenezes <erickmenezes.dev@gmail.com>
+     * @author ErickJMenezes <erickmenezes.dev@gmail.com>
      */
-    public static function make(string $interface, array $options = []): object
+    public static function make(string $interfaceName, array $options = []): object
     {
         // Instantiate the client factory with all dependencies.
 
+        $reflectedInterface = new ReflectionClass($interfaceName);
+
         return (new self(
-            new ReflectionClass($interface),
+            $reflectedInterface,
             new AnonymousClassGenerator(),
-            new MethodInvoker(
-                new ParameterReader(),
-                new ResponseParser(),
-                new GuzzleClient($options),
+            new Proxy(
+                $reflectedInterface,
+                new MethodInvoker(
+                    new ParameterReader(),
+                    new ResponseParser(),
+                    new GuzzleClient($options),
+                )
             )
         ))
             ->makeImplementation();
     }
 
     /**
+     * @throws \Exception
+     */
+    public function makeImplementation(): mixed
+    {
+        return $this->anonymousClassGenerator
+            ->instantiateFromReflection($this->interface, $this->proxy);
+    }
+
+    /**
      * @throws \InvalidArgumentException
      */
-    private function validateInterfaceName(): void
+    private function validateInterface(): void
     {
         if (!$this->interface->isInterface()) {
             $this->throwInvalidClassStringException($this->interface->getName());
@@ -99,14 +91,5 @@ class Factory implements MethodCallHandler, ClientFactory
         throw new InvalidArgumentException(
             "The value \"$interface\" is not a valid fully qualified interface name.", 10
         );
-    }
-
-    /**
-     * @throws \Exception
-     */
-    public function makeImplementation(): mixed
-    {
-        return $this->anonymousClassGenerator
-            ->instantiateFromReflection($this->interface, $this);
     }
 }
---
+++ src/Waffler/Client/Proxy.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Waffler\Client;
+
+use BadMethodCallException;
+use Waffler\Generator\Contracts\MethodCallHandler;
+
+/**
+ * Class Proxy.
+ *
+ * This class proxies all calls form the anonymous implementation to the real method.
+ *
+ * @author ErickJMenezes <erickmenezes.dev@gmail.com>
+ * @phpstan-template TInterfaceType of object
+ */
+class Proxy implements MethodCallHandler
+{
+    /**
+     * @param \ReflectionClass<TInterfaceType> $interface
+     * @param \Waffler\Client\MethodInvoker    $methodInvoker
+     */
+    public function __construct(
+        private \ReflectionClass $interface,
+        private MethodInvoker $methodInvoker
+    ) {
+    }
+
+    /**
+     * @throws \ReflectionException
+     * @throws \Exception
+     */
+    public function __call(string $name, array $arguments): mixed
+    {
+        if (!$this->interface->hasMethod($name)) {
+            $this->throwUndefinedMethodError($name);
+        }
+
+        return $this->methodInvoker->invoke($this->interface->getMethod($name), $arguments);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @author ErickJMenezes <erickmenezes.dev@gmail.com>
+     */
+    private function throwUndefinedMethodError(string $name): void
+    {
+        throw new BadMethodCallException(
+            "The method $name is not declared in {$this->interface->getName()}."
+        );
+    }
+}
